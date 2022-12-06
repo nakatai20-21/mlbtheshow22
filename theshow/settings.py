d771601d2e304cc39125228cbb9e502a72700132
naktai21 <nakatai15@stu.kanazawa-u.ac.jp>
2022-12-06 14:50:17 +0900

    first commit

--- theshow/settings.py
+++ theshow/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(lh$c1_@r8x#l67tn+ugq5j_rblukn=ya$h6lo)-@p!-dc9a%k'
+from .local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
